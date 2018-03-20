14345e25964ad9d6e27786b75aa479bfec0dec0f
MeanOldTree <meanoldtree2@gmail.com>
2018-03-20 13:24:04 -0400

    updated the script after a test run with a couple of comments. also added a clear before the finish message to bounce the screen and let the user know that its finished

--- medusa-install.sh
+++ medusa-install.sh
@@ -1,19 +1,27 @@
  #!/bin/bash
 clear
+# ~~~ This section Depreciated but left to help understand the process
 # set some variables
 #USER="medusa"
 #DIR="/opt/medusa"
 #then to get some input to check them
+# ~~~ End Section
+
+#Read into  variables some importand pieces the user needs to be able to change. 
+#another thing to add here would be a choice between a DEB/RPM/APK based system install.
+# Need to look into seeing if that can be autodetected. 
 read -e -p "Please enter a name for the Medusa User: " -i medusa -e  USER
 read -e -p "Please enter a directory to store Medusa: " -i /opt/medusa -e DIR
 
 echo " So Medusa is being installed in $DIR as the user $USER"
 read -p " sound about rignt? [Y/n]" -n 1 -r
-echo    # (optional) move to a new line
+echo    
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 
 fi
+
+# handle exits from shell or function but don't exit interactive shell, neat little code snippit.
 
 
 # install some prerequisits
@@ -33,5 +41,7 @@ sudo chmod 644 /etc/systemd/system/medusa.service
 # and start it 
 sudo systemctl enable medusa
 sudo systemctl start medusa
-
+#clean the garbage off the screen
+clear
 echo "All Set, medusa is up and running on port 8081. Enjoy!"
+#and thats it. simple little script but it makes deploying things like this a lot less time consuming.
